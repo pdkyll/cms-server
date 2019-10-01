f907b8911fa1f6bf9deeccfc3771a5f9ffb25cde
Menno Menninga <menno.menninga@ovsoftware.com>
2019-10-02 00:28:22 +0200

    Added only_menu field to native queries.

--- src/main/java/nl/menninga/menno/cms/repository/CmsObjectRepository.java
+++ src/main/java/nl/menninga/menno/cms/repository/CmsObjectRepository.java
@@ -17,32 +17,32 @@ import nl.menninga.menno.cms.entity.CmsObject;
 public interface CmsObjectRepository extends JpaRepository<CmsObject, Long> {
 
 	@Query(nativeQuery=true, value=
-			"WITH RECURSIVE cms_object_temp (id, creation_time, modification_time, name, name_path, view, published, child_index, parent_id, json, search_string, level, path_array) AS (" + 
-			"    SELECT id, creation_time, modification_time, name, name_path, view, published, child_index, parent_id, json, search_string, 0, ARRAY[name_path]" + 
+			"WITH RECURSIVE cms_object_temp (id, creation_time, modification_time, name, name_path, view, only_menu, published, child_index, parent_id, json, search_string, level, path_array) AS (" + 
+			"    SELECT id, creation_time, modification_time, name, name_path, view, only_menu, published, child_index, parent_id, json, search_string, 0, ARRAY[name_path]" + 
 			"    FROM cms_object" + 
 			"    WHERE parent_id is null" + 
 			"    UNION ALL" + 
-			"    SELECT c.id, c.creation_time, c.modification_time, c.name, c.name_path, c.view, c.published, c.child_index, c.parent_id, c.json, c.search_string, (t0.level + 1), ARRAY_APPEND(t0.path_array, c.name_path)" + 
+			"    SELECT c.id, c.creation_time, c.modification_time, c.name, c.name_path, c.view, c.only_menu, c.published, c.child_index, c.parent_id, c.json, c.search_string, (t0.level + 1), ARRAY_APPEND(t0.path_array, c.name_path)" + 
 			"    FROM cms_object c" + 
 			"            INNER JOIN cms_object_temp t0 ON t0.id = c.parent_id" + 
 			") " +
-			"SELECT id, creation_time, modification_time, name, name_path, view, published, child_index, parent_id, json, search_string " + 
+			"SELECT id, creation_time, modification_time, name, name_path, view, only_menu, published, child_index, parent_id, json, search_string " + 
 			"FROM cms_object_temp " + 
 			"WHERE ARRAY_TO_STRING(path_array, '/') = :path"
 	)
 	CmsObject findOneByPath(@Param("path") String path);
 
 	@Query(nativeQuery=true, value=
-			"WITH RECURSIVE cms_object_temp (id, creation_time, modification_time, name, name_path, view, published, child_index, parent_id, json, search_string, level, path_array) AS (" + 
-			"    SELECT id, creation_time, modification_time, name, name_path, view, published, child_index, parent_id, json, search_string, 0, ARRAY[name_path]" + 
+			"WITH RECURSIVE cms_object_temp (id, creation_time, modification_time, name, name_path, view, only_menu, published, child_index, parent_id, json, search_string, level, path_array) AS (" + 
+			"    SELECT id, creation_time, modification_time, name, name_path, view, only_menu, published, child_index, parent_id, json, search_string, 0, ARRAY[name_path]" + 
 			"    FROM cms_object" + 
 			"    WHERE parent_id is null AND published = :published" + 
 			"    UNION ALL" + 
-			"    SELECT c.id, c.creation_time, c.modification_time, c.name, c.name_path, c.view, c.published, c.child_index, c.parent_id, c.json, c.search_string, (t0.level + 1), ARRAY_APPEND(t0.path_array, c.name_path)" + 
+			"    SELECT c.id, c.creation_time, c.modification_time, c.name, c.name_path, c.view, c.only_menu, c.published, c.child_index, c.parent_id, c.json, c.search_string, (t0.level + 1), ARRAY_APPEND(t0.path_array, c.name_path)" + 
 			"    FROM cms_object c" + 
 			"            INNER JOIN cms_object_temp t0 ON t0.id = c.parent_id" + 
 			") " +
-			"SELECT id, creation_time, modification_time, name, name_path, view, published, child_index, parent_id, json, search_string " + 
+			"SELECT id, creation_time, modification_time, name, name_path, view, only_menu, published, child_index, parent_id, json, search_string " + 
 			"FROM cms_object_temp " + 
 			"WHERE ARRAY_TO_STRING(path_array, '/') = :path AND published = :published"
 	)
@@ -63,12 +63,12 @@ public interface CmsObjectRepository extends JpaRepository<CmsObject, Long> {
 
 	@Modifying
 	@Query(nativeQuery=true, value=
-			"WITH RECURSIVE cms_object_temp (id, creation_time, modification_time, name, name_path, view, published, child_index, parent_id, json, search_string, level, path_array) AS (" + 
-			"    SELECT id, creation_time, modification_time, name, name_path, view, published, child_index, parent_id, json, search_string, 0, ARRAY[name_path]" + 
+			"WITH RECURSIVE cms_object_temp (id, creation_time, modification_time, name, name_path, view, only_menu, published, child_index, parent_id, json, search_string, level, path_array) AS (" + 
+			"    SELECT id, creation_time, modification_time, name, name_path, view, only_menu, published, child_index, parent_id, json, search_string, 0, ARRAY[name_path]" + 
 			"    FROM cms_object" + 
 			"    WHERE parent_id is null" + 
 			"    UNION ALL" + 
-			"    SELECT c.id, c.creation_time, c.modification_time, c.name, c.name_path, c.view, c.published, c.child_index, c.parent_id, c.json, c.search_string, (t0.level + 1), ARRAY_APPEND(t0.path_array, c.name_path)" + 
+			"    SELECT c.id, c.creation_time, c.modification_time, c.name, c.name_path, c.view. c.only_menu, c.published, c.child_index, c.parent_id, c.json, c.search_string, (t0.level + 1), ARRAY_APPEND(t0.path_array, c.name_path)" + 
 			"    FROM cms_object c" + 
 			"            INNER JOIN cms_object_temp t0 ON t0.id = c.parent_id" + 
 			") " +
